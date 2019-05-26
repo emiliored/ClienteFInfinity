62a9ba87bc1d7c114eccab9d69fbb63b92f6cb79
emipley <35601490+emipley@users.noreply.github.com>
2019-05-26 13:31:13 +0200

    Octavo commit
    Atrancado con la lista RecursoCliente, no deserializa bien

--- src/conexion/EtiquetaUsuario.java
+++ src/conexion/EtiquetaUsuario.java
@@ -7,6 +7,8 @@ package conexion;
 
 import static conexion.Conectar.IPSERVER;
 import conexion.objetos.Etiqueta;
+import conexion.objetos.Recurso;
+import conexion.objetos.RecursoCliente;
 import java.util.List;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -19,11 +21,42 @@ import javax.ws.rs.core.MediaType;
  */
 public class EtiquetaUsuario {
     
-    public static List<Etiqueta> obtenerEtiquetasUsuarios(){
+     public static List<Etiqueta> obtenerEtiquetasNovedades(){
         Client client=ClientBuilder.newClient();
         return (List<Etiqueta>) client.target(
-             "http://"+IPSERVER+":8080/ServidorFInfinity/servicios/etiqueta" )
+             "http://"+IPSERVER+":8080/ServidorFInfinity/servicios/etiqueta/novedades")
                 .request(MediaType.APPLICATION_JSON).get(new GenericType<List<Etiqueta>>(){});
        
     }
+    
+    public static List<Etiqueta> obtenerEtiquetasGenerales(){
+        Client client=ClientBuilder.newClient();
+        return (List<Etiqueta>) client.target(
+             "http://"+IPSERVER+":8080/ServidorFInfinity/servicios/etiqueta/generales" )
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<List<Etiqueta>>(){});
+       
+    }
+    public static List<RecursoCliente> obtenerRecursoSinEtiquetar(){
+        Client client=ClientBuilder.newClient();
+        return (List<RecursoCliente>) client.target(
+             "http://"+IPSERVER+":8080/ServidorFInfinity/servicios/recurso/sinetiquetar")
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<List<RecursoCliente>>(){});
+    }
+
+    public static List<String> obtenerEtiquetasUsuarioPublicas(int idUsuario){
+        Client client=ClientBuilder.newClient();
+        return (List<String>) client.target(
+             "http://"+IPSERVER+":8080/ServidorFInfinity/servicios/etiqueta/publicas?idUsuario="+idUsuario)
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<List<String>>(){});
+       
+    }
+    
+    public static List<String> obtenerEtiquetasUsuarioPrivadas(int idUsuario){
+        Client client=ClientBuilder.newClient();
+        return (List<String>) client.target(
+             "http://"+IPSERVER+":8080/ServidorFInfinity/servicios/etiqueta/privadas?idUsuario="+idUsuario)
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<List<String>>(){});
+       
+    }
+    
 }
--- src/controladores/BaseController.java
+++ src/controladores/BaseController.java
@@ -13,7 +13,6 @@ import com.pepperonas.fxiconics.MaterialColor;
 import com.pepperonas.fxiconics.gmd.FxFontGoogleMaterial;
 import com.sun.javafx.font.Glyph;
 import conexion.EtiquetaUsuario;
-import static conexion.EtiquetaUsuario.obtenerEtiquetasUsuarios;
 import conexion.FicherosBinarios;
 import conexion.RecursoClase;
 import static conexion.RecursoClase.obtenerRecursosPorId;
@@ -74,6 +73,8 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.faces.flow.Flow;
 import static javax.ws.rs.client.Entity.text;
+import static conexion.EtiquetaUsuario.obtenerEtiquetasGenerales;
+import conexion.objetos.RecursoCliente;
 
 /**
  * FXML Controller class
@@ -82,25 +83,26 @@ import static javax.ws.rs.client.Entity.text;
  */
 public class BaseController implements Initializable {
 
-    
     @FXML
     private Accordion acordeonIzq;
-    @FXML
-    private TitledPane POPULARES;
-    @FXML
-    private Label lblApodoUsuario;
-    @FXML
-    private FlowPane flow;
-
-    ListView<String> lista = new ListView<>();
-    TextField textComentar = new TextField();
-    Parent parent;
-    private Stage stage;
-
     @FXML
     private BorderPane border;
     @FXML
     private Button btnAnadirRecurso;
+    @FXML
+    private Label lblApodoUsuario;
+    @FXML
+    private FlowPane flow;
+    @FXML
+    private Accordion acordeonDer;
+    @FXML
+    private StackPane baseStack;
+    @FXML
+    private JFXButton btnAnadirRecEnBase;
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private TitledPane POPULARES;
     @FXML
     private TitledPane MASVALORADAS;
     @FXML
@@ -110,10 +112,15 @@ public class BaseController implements Initializable {
     @FXML
     private TitledPane SINETIQUETAR;
     @FXML
-    private Accordion acordeonDer;
+    private TitledPane PUBLICAS;
     @FXML
-    private StackPane baseStack;
+    private TitledPane PRIVADAS;
+
     //------------------------------
+    ListView<String> lista = new ListView<>();
+    TextField textComentar = new TextField();
+    Parent parent;
+    private Stage stage;
     //Vista HBox recurso
     private HBox vistaRecursos;
     private Label lblApodo;
@@ -121,67 +128,123 @@ public class BaseController implements Initializable {
     private Label lblDescripcion;
     private JFXButton btnVer;
     //-----------------------------
-    @FXML
-    private JFXButton btnAnadirRecEnBase;
-    @FXML
-    private ScrollPane scrollPane;
-    
 
-
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-        
-        
-            lblApodoUsuario.setText(superNombre);
-            //recibeParametros(text);
-            acordeonIzq.expandedPaneProperty().addListener(
-                    (ObservableValue<? extends TitledPane> ov, TitledPane old_val,
-                            TitledPane new_val) -> {
-                        if (new_val != null) {
-                            prueba();
-                        }
+        //System.out.println(EtiquetaUsuario.obtenerRecursoSinEtiquetar());
+        lblApodoUsuario.setText(superNombre);
+        //recibeParametros(text);
+        acordeonIzq.expandedPaneProperty().addListener(
+                (ObservableValue<? extends TitledPane> ov, TitledPane old_val,
+                        TitledPane new_val) -> {
+                    if (new_val != null) {
+                        cargarListasTags(new_val);
                     }
-            );
-//        try {    
-            //recurso();
-            //cargarListaRecursos();
-//        } catch (IOException ex) {
-//            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+                }
+        );
+        acordeonDer.expandedPaneProperty().addListener(
+                (ObservableValue<? extends TitledPane> ov, TitledPane old_val,
+                        TitledPane new_val) -> {
+                    if (new_val != null) {
+                        cargarListasTags(new_val);
+                    }
+                }
+        );
+        //recurso();          
+
     }
 
     //Métodos nuestros.
-    public void prueba() {
-        VBox contenido = new VBox();
-        contenido.setStyle("-fx-background-color:#EAB0B2;");
-        Label l;
+    public void cargarListasTags(TitledPane tp) {
 
-        List<Etiqueta> lista = EtiquetaUsuario.obtenerEtiquetasUsuarios();
+        int i = 3;    //IDUSUARIO recuperarlo-----------------------------------------*********
+        VBox contenido = null;
+        //Switch para cargar el TitledPane apropiado.
+        System.out.println(tp.getText());
+        switch (tp.getText()) {
+            case "POPULARES":   //TODO
+                //contenido=configurarListaEtiquetas(EtiquetaUsuario.obtenerEtiquetasPopulares());
+                break;
+            case "MASVALORADAS":    //TODO
+                //contenido=configurarListaEtiquetas(EtiquetaUsuario.obtenerEtiquetasValoradas());
+                break;
+            case "NOVEDADES":
+                contenido = configurarListaEtiquetas(EtiquetaUsuario.obtenerEtiquetasNovedades());
+                break;
+            case "GENERAL":
+                contenido = configurarListaEtiquetas(EtiquetaUsuario.obtenerEtiquetasGenerales());
+                break;
+            case "SIN ETIQUETAR":
+                contenido = configurarListaEtiquetas(EtiquetaUsuario.obtenerRecursoSinEtiquetar());
+                break;
+            case "PUBLICAS":
+                contenido = configurarListaEtiquetas(EtiquetaUsuario.obtenerEtiquetasUsuarioPublicas(i));
+                break;
+            case "PRIVADAS":
+                contenido = configurarListaEtiquetas(EtiquetaUsuario.obtenerEtiquetasUsuarioPrivadas(i));
+                break;
+
+        }
+        tp.setContent(contenido);
         //System.out.println(lista);
 
-        for (Etiqueta e : lista) {
-            l = (new Label("#" + e.getEtiquetaPK().getNombre()));
-            l.setOnMouseClicked((new EventHandler<MouseEvent>() {
+    }
 
-                @Override
-                public void handle(MouseEvent event) {
-                    if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
-                        
-                        System.out.println("Hola");
+    private VBox configurarListaEtiquetas(List<?> lista) {
+        Label l = null;
+        VBox contenido = new VBox();
+        contenido.setStyle("-fx-background-color:#EAB0B2;");
+
+        for (Object o : lista) {
+            if (o instanceof Etiqueta) {
+                Etiqueta e = (Etiqueta) o;
+                l = (new Label("#" + e.getEtiquetaPK().getNombre()));
+                l.setOnMouseClicked((new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+
+                            System.out.println("Hola");
+                        }
                     }
-                }
-            }));
+                }));
+            }
+             if (o instanceof String) { //PUBLICAS / PRIVADAS
+                String s = (String) o;
+                l = new Label("#" + s);
+                l.setOnMouseClicked((new EventHandler<MouseEvent>() {
 
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+
+                            System.out.println("Hola");
+                        }
+                    }
+                }));
+            }
+             
+                if (o instanceof RecursoCliente) {  //SIN ETIQUETAR
+                    RecursoCliente rc = (RecursoCliente) o;
+                    l = new Label("#" + rc.getNombre().substring(0, rc.getNombre().lastIndexOf(".")));
+                    l.setOnMouseClicked((new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+                                //addGrid(rc.getIdRecurso());
+                            }
+                        }
+                    }));
+                }
             contenido.getChildren().add(l);
         }
-        POPULARES.setContent(contenido);
+        return contenido;
     }
-    
+
     public void recurso() {
-        
-        
+
         HBox hboxMultiple;
         AnchorPane caja;
 
@@ -193,20 +256,18 @@ public class BaseController implements Initializable {
         for (Recurso r : listaRecursos) {
             caja = new AnchorPane();
             hboxMultiple = new HBox();
-            caja.setPrefSize(801, 128);            
+            caja.setPrefSize(801, 128);
             caja.setStyle("-fx-background-color: #FF6B6B;");
-            
+
             hboxMultiple.setPrefSize(801, 128);
             hboxMultiple.setSpacing(5);
-            hboxMultiple.setStyle("-fx-background-color: #AA4747;");            
+            hboxMultiple.setStyle("-fx-background-color: #AA4747;");
             hboxMultiple.setPadding(new Insets(30, 0, 0, 0));
             AnchorPane.setTopAnchor(hboxMultiple, 10.0);
             AnchorPane.setLeftAnchor(hboxMultiple, 10.0);
             AnchorPane.setRightAnchor(hboxMultiple, 10.0);
             AnchorPane.setBottomAnchor(hboxMultiple, 10.0);
-            
-           
-            
+
             FxIconicsLabel labIcon = (FxIconicsLabel) new FxIconicsLabel.Builder(FxFontGoogleMaterial.Icons.gmd_folder_special)
                     .size(30)
                     //.text("ARCHIVO")
@@ -214,20 +275,17 @@ public class BaseController implements Initializable {
                     .build();
             labIcon.setPadding(new Insets(-10, 0, 0, 10));//public Insets(double top,double right,double bottom,double left)
             hboxMultiple.getChildren().add(labIcon);
-            
+
             JFXButton btVer = new JFXButton();
             btVer.setText("VER");
             btVer.setPrefSize(104, 30);
             //btVer.setPadding(new Insets(0, 0, 0, 0));
             btVer.setStyle("-fx-text-fill: #000000;"
-                            +"-fx-background-color: #ffffff;"
-                            + "-fx-font-size: 15px;\n" 
-                            +"-fx-font-family: \"System\"");
-            btVer.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent e) {
-                    addGrid(r.getIdRecurso());
-                }
+                    + "-fx-background-color: #ffffff;"
+                    + "-fx-font-size: 15px;\n"
+                    + "-fx-font-family: \"System\"");
+            btVer.setOnAction((ActionEvent e) -> {
+                addGrid(r.getIdRecurso());
             });
 
             hboxMultiple.getChildren().add(btVer);
@@ -235,24 +293,24 @@ public class BaseController implements Initializable {
             lbApodo.setPrefSize(90, 35);
             lbApodo.setPadding(new Insets(0, 0, 0, 20));
             lbApodo.setStyle("-fx-text-fill: #ffffff;"
-                            + "-fx-font-size: 15px;\n" +
-                            "-fx-font-family: \"System\"");
+                    + "-fx-font-size: 15px;\n"
+                    + "-fx-font-family: \"System\"");
             hboxMultiple.getChildren().add(lbApodo);
 
             Label lbRecurso = new Label(r.getNombre());
             lbRecurso.setPrefSize(140, 35);
             lbRecurso.setPadding(new Insets(0, 0, 0, 20));
             lbRecurso.setStyle("-fx-text-fill: #ffffff;"
-                            + "-fx-font-size: 15px;\n" +
-                            "-fx-font-family: \"System\"");
+                    + "-fx-font-size: 15px;\n"
+                    + "-fx-font-family: \"System\"");
             hboxMultiple.getChildren().add(lbRecurso);
 
             Label lblDescripcion = new Label(r.getDescripcion());
             lblDescripcion.setPrefSize(340, 58);
             lblDescripcion.setPadding(new Insets(40, 10, 10, 20));
             lblDescripcion.setStyle("-fx-text-fill: #ffffff;"
-                            + "-fx-font-size: 15px;\n" +
-                            "-fx-font-family: \"System\"");
+                    + "-fx-font-size: 15px;\n"
+                    + "-fx-font-family: \"System\"");
             lblDescripcion.setAlignment(Pos.CENTER);
             hboxMultiple.getChildren().add(lblDescripcion);
             caja.getChildren().add(hboxMultiple);
@@ -263,7 +321,7 @@ public class BaseController implements Initializable {
     }
 
     public void addGrid(int idRecurso) {
-        
+
         GridPane gridRecurso = new GridPane();
         Recurso recurso = obtenerRecursosPorId(idRecurso);
 
@@ -304,7 +362,7 @@ public class BaseController implements Initializable {
         title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         vbox.getChildren().add(title);
 
-        List<Etiqueta> listaEtiquetas = obtenerEtiquetasUsuarios();
+        List<Etiqueta> listaEtiquetas = obtenerEtiquetasGenerales();
         Hyperlink options[] = new Hyperlink[]{
             new Hyperlink("#" + "playa"),
             new Hyperlink("montaña"),
@@ -333,10 +391,10 @@ public class BaseController implements Initializable {
         descargar.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                     FileChooser fileChooser = new FileChooser();
-              //Show save file dialog
-              File file = fileChooser.showSaveDialog(stage);
-              FicherosBinarios.descargar(recurso.getIdRecurso(),file);
+                FileChooser fileChooser = new FileChooser();
+                //Show save file dialog
+                File file = fileChooser.showSaveDialog(stage);
+                FicherosBinarios.descargar(recurso.getIdRecurso(), file);
             }
         });
         gridRecurso.add(descargar, 0, 3);
@@ -379,7 +437,7 @@ public class BaseController implements Initializable {
         hb.getChildren().addAll(lista);
         gridRecurso.add(hb, 0, 5, 3, 4);
         flow.getChildren().add(gridRecurso);
-        
+
     }
 
     private void comentario() {
@@ -410,25 +468,25 @@ public class BaseController implements Initializable {
     }
 
     @FXML
-    private void anadirRecurso(ActionEvent event){
-        
+    private void anadirRecurso(ActionEvent event) {
+
         recurso();
-    }   
-    
-    public void cargarListaRecursos() throws IOException {          
+    }
+
+    public void cargarListaRecursos() throws IOException {
 
         AnchorPane hboxMultiple;
-        Label a=new Label();
-        Label b=new Label();
-        Label c=new Label();
+        Label a = new Label();
+        Label b = new Label();
+        Label c = new Label();
         List<Recurso> listaRecursos = RecursoClase.obtenerRecursos();
         System.out.println(listaRecursos);
         flow.getChildren().removeAll();
-        flow.getChildren().clear();        
+        flow.getChildren().clear();
 
-        for (Recurso r : listaRecursos){             
-            
-            a.setText(r.getNombre());            
+        for (Recurso r : listaRecursos) {
+
+            a.setText(r.getNombre());
             b.setText(r.getDescripcion());
             c.setText(Integer.toString(r.getIdUsuario()));
 //            btnVer.setText("VER");
@@ -443,16 +501,15 @@ public class BaseController implements Initializable {
             //hboxMultiple = FXMLLoader.load(getClass().getResource("/fxml/BoxRecurso.fxml")); 
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PanelCentral.fxml"));
             hboxMultiple = loader.load();
-            PanelCentralController controller = (PanelCentralController)loader.getController();
-            controller.setLabelText(a.getText(), b.getText(),c.getText());
+            PanelCentralController controller = (PanelCentralController) loader.getController();
+            controller.setLabelText(a.getText(), b.getText(), c.getText());
             flow.getChildren().add(hboxMultiple);
         }
-        
-    }
-    
 
-    public void ojo(Parent papa) throws IOException{
-        parent = FXMLLoader.load(getClass().getResource("/fxml/AnadirRecurso.fxml"));        
+    }
+
+    public void ojo(Parent papa) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("/fxml/AnadirRecurso.fxml"));
         flow.getChildren().clear();
         flow.getChildren().add(papa);
         border.setCenter(papa);
@@ -460,8 +517,8 @@ public class BaseController implements Initializable {
 
     @FXML
     private void AnadirRecEnBase(ActionEvent event) throws IOException {
-        
-        AnchorPane anchor = FXMLLoader.load(getClass().getResource("/fxml/AnadirRecurso.fxml"));        
+
+        AnchorPane anchor = FXMLLoader.load(getClass().getResource("/fxml/AnadirRecurso.fxml"));
         flow.getChildren().clear();
         flow.getChildren().add(anchor);
         //border.setCenter(anchor);      
